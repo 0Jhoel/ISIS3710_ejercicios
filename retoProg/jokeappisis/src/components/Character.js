5f232b72e9357477350df661df7e162c08cec407
0Jhoel <jj.barriosm@uniandes.edu.co>
2021-05-11 14:23:05 -0500

    now it shows the image

--- retoProg/jokeappisis/src/components/Character.js
+++ retoProg/jokeappisis/src/components/Character.js
@@ -2,8 +2,14 @@ import {useState,useEffect} from "react";
 
 const Character = () =>{
 
+	function getRandomInt(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
 	const [character,setCharacter] = useState();
 	useEffect(()=>{
+		var totalCharacters = 1478;
+		var characterOffset = getRandomInt(0, totalCharacters);
 		if(!navigator.onLine){
 			if(localStorage.getItem("Character") === null){
 				setCharacter("Loading…");
@@ -13,19 +19,23 @@ const Character = () =>{
 			}
 		}
 		fetch("http://gateway.marvel.com/v1/public/characters/1011334?ts=1&apikey=52c480118bb7817c642c345e56def1ca&hash=5b4071d6b10b8e22cab403be946857c6")
-		.then(result=>result.json)
-        .then(result=>{
-		console.log("result",result);
-		localStorage.setItem("Character",result.value);
-		setCharacter(result.value);
-		
+		.then((result)=>result.json())
+        .then((result)=>{
+			var data = result.data.results[0];
+			var img = data.thumbnail.path + "."+data.thumbnail.extension
+			localStorage.setItem("Character",data);
+			setCharacter({...data,thumbnail:{...data.thumbnail,path:img}});
+			console.log(character);
 		});
 	},[]);
 
 	return(
         <div>
             <h1>Character</h1>
-            <p>{character}</p>
+            <p>Name: {character.name}</p>
+			<p>Id: {character.id}</p>
+			<p>Biography: {character.description}</p>
+			<img src={character.thumbnail.path} alt="character"/>
         </div>
 	);
 };
