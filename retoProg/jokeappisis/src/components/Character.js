fffabb90e9c9203b1dd8c5d4760afbea6267e591
0Jhoel <jj.barriosm@uniandes.edu.co>
2021-05-11 23:47:41 -0500

    final commit with image caching

--- retoProg/jokeappisis/src/components/Character.js
+++ retoProg/jokeappisis/src/components/Character.js
@@ -2,40 +2,57 @@ import {useState,useEffect} from "react";
 
 const Character = () =>{
 
-	function getRandomInt(min, max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
+	const [character,setCharacter] = useState();
+
+	const getRandomInt = (min, max) => {
+		var r = Math.floor(Math.random() * (max - min + 1)) + min;
+		return r.toString()
 	}
 
-	const [character,setCharacter] = useState();
+	const toDataURL = url => fetch(url)
+	.then(response => response.blob())
+	.then(blob => new Promise((resolve, reject) => {
+		const reader = new FileReader()
+		reader.onloadend = () => resolve(reader.result)
+		reader.onerror = reject
+		reader.readAsDataURL(blob)
+	}));
+
 	useEffect(()=>{
-		var totalCharacters = 1478;
-		var characterOffset = getRandomInt(0, totalCharacters);
 		if(!navigator.onLine){
-			if(localStorage.getItem("Character") === null){
+			if(localStorage.getItem("character") === null){
 				setCharacter("Loading…");
 			}
 			else{
-                setCharacter(localStorage.getItem("Character"));
+                setCharacter(localStorage.getItem("character"));
 			}
 		}
-		fetch("http://gateway.marvel.com/v1/public/characters/1011334?ts=1&apikey=52c480118bb7817c642c345e56def1ca&hash=5b4071d6b10b8e22cab403be946857c6")
+		fetch("http://gateway.marvel.com/v1/public/characters/"+getRandomInt(1011300, 1011400)+"?ts=1&apikey=52c480118bb7817c642c345e56def1ca&hash=5b4071d6b10b8e22cab403be946857c6")
 		.then((result)=>result.json())
         .then((result)=>{
 			var data = result.data.results[0];
-			var img = data.thumbnail.path + "."+data.thumbnail.extension
-			localStorage.setItem("Character",data);
+			var img = data.thumbnail.path + "."+data.thumbnail.extension;
 			setCharacter({...data,thumbnail:{...data.thumbnail,path:img}});
-			console.log(character);
+			toDataURL(img)
+			.then(dataUrl => {
+				localStorage.setItem("character",{...data,thumbnail:{...data.thumbnail,path:dataUrl}});
+			})
 		});
 	},[]);
 
-	return(
+	return (
         <div>
-            <h1>Character</h1>
-            <p>Name: {character.name}</p>
-			<p>Id: {character.id}</p>
-			<p>Biography: {character.description}</p>
-			<img src={character.thumbnail.path} alt="character"/>
+			{character ? (
+				<div>
+					<h1>Character</h1>
+            		<p>Name: {character.name}</p>
+					<p>Id: {character.id}</p>
+					<p>Biography: {character.description}</p>
+					<img src={character.thumbnail.path} alt="character"/>
+				</div>
+			): (
+				<p>Loading...</p>
+			  )}
         </div>
 	);
 };
